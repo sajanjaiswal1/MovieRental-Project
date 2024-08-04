e1613668c8df2e9167cb85f39783e2420807c023
sajanjaiswal1 <jaiswalsandip88@yahoo.com>
2024-08-04 17:36:09 +0545

    Prepare for deployment

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
 import "./App.css";
 import MyRoutes from "./MyRoutes";
 import { persistStore, persistReducer } from "redux-persist";
-import storage from "redux-persist/lib/storage"; // defaults to localStorage for web
+import storage from "redux-persist/lib/storage";
 import { PersistGate } from "redux-persist/integration/react";
 import { applyMiddleware, combineReducers, createStore } from "redux";
 import { movieReducer } from "./assets/Redux/movieReducer";
 import { cartReducer } from "./assets/Redux/cartReducers";
-import { Provider, useDispatch } from "react-redux";
-import { useEffect } from "react";
+import { Provider} from "react-redux";
 import { thunk } from "redux-thunk";
 import userInfoReducer from "./assets/Redux/userInfoReducer";
 
